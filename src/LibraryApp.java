0eb529f8d403c4c0025c5a3188e4befa597b5dc8
s-saratov <saratovs@yandex.ru>
2024-10-29 11:47:57 +0100

    classes add

---
+++ src/LibraryApp.java
@@ -0,0 +1,9 @@
+public class LibraryApp {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/repository/BookRepositoryImpl.java
@@ -0,0 +1,4 @@
+package repository;
+
+public class BookRepositoryImpl implements BookRepository{
+}
