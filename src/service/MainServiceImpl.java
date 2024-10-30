9fe734c4562d39d2d5d12af111694a1117b1214b
s-saratov <saratovs@yandex.ru>
2024-10-30 12:11:00 +0100

    MainService add

--- src/service/MainServiceImpl.java
+++ src/service/MainServiceImpl.java
@@ -1,4 +1,109 @@
 package service;
 
+import model.User;
+import utils.MyList;
+
+import java.time.LocalDate;
+
 public class MainServiceImpl implements MainService{
+
+    @Override
+    public void addBook(String author, String title, int year, String publisher) {
+
+    }
+
+    @Override
+    public User registerUser(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getAllBooksSortedByAuthor(String author) {
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getAllBooksSortedByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public Book getBookByID(int id) {
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getBooksByAuthor(String author) {
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getFreeBooks() {
+        return null;
+    }
+
+    @Override
+    public int borrowingTerm(int id) {
+        return 0;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User getActiveUser() {
+        return null;
+    }
+
+    @Override
+    public MyList<User> getUsersByRole(Role... roles) {
+        return null;
+    }
+
+    @Override
+    public String getBorrowersEmail(int id) {
+        return "";
+    }
+
+    @Override
+    public boolean takeBook(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean takeBook(int id, LocalDate NewDate) {
+        return false;
+    }
+
+    @Override
+    public boolean updateBorrowDate(int id, LocalDate NewBorrowDate) {
+        return false;
+    }
+
+    @Override
+    public boolean returnBook(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean loginUser(String email, String password) {
+        return false;
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public Book deleteBookByID(int id) {
+        return null;
+    }
 }
