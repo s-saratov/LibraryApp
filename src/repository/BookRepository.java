c2b2da558b369ff3c1bfae347fbf7984de9614cb
volkde <ilyaprofit@gmail.com>
2024-10-30 11:53:36 +0100

    add book repositiry

--- src/repository/BookRepository.java
+++ src/repository/BookRepository.java
@@ -2,6 +2,16 @@ package repository;
 
 public interface BookRepository {
 
-  void addBook(String name, int year, double price);
+  //Добавление книги в общий список
+  void addBook(String author, String title, int year, String publisher);
 
+  //Получение информации о книгах
+  MyList<Book> getAllBooks();
+  Book getByID(int id);
+  MyList<Book> getBooksByTitle(String title);
+  MyList<Book> getBooksByAuthor(String author);
+  MyList<Book> getFreeBooks();
+
+  //Удаление книги
+  void deleteBook(Book book);
 }
