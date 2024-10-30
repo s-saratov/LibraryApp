9097215863f16a60cf62067c0e49b92c40da4d91
Maksym Stoyanov <sto.maks+github.com@gmail.com>
2024-10-30 11:30:34 +0100

    add: basic UserRepository and UserRepositoryImpl

--- src/repository/UserRepositoryImpl.java
+++ src/repository/UserRepositoryImpl.java
@@ -1,4 +1,57 @@
 package repository;
 
-public class UserRepositoryImpl implements UserRepository{
+import model.Role;
+import model.User;
+import utils.MyList;
+
+public class UserRepositoryImpl implements UserRepository {
+
+  /**
+   * Добавляет нового пользователя с указанной электронной почтой и паролем в репозиторий.
+   *
+   * @param email    Электронная почта пользователя. Должна быть уникальной в системе.
+   * @param password Пароль пользователя.
+   * @return Объект пользователя {@code User}.
+   */
+  @Override
+  public User addUser(String email, String password) {
+    return null;
+  }
+
+
+  /**
+   * Проверяет, существует ли пользователь с заданным адресом электронной почты.
+   *
+   * @param email Электронная почта для проверки.
+   * @return {@code true}, если пользователь с указанным адресом существует; {@code false} иначе.
+   */
+  @Override
+  public boolean isEmailExists(String email) {
+    return false;
+  }
+
+
+  /**
+   * Возвращает список пользователей, имеющих указанные роли.
+   *
+   * @param roles Роли, по которым осуществляется фильтрация пользователей.
+   * @return {@code MyList<User>} Список пользователей, соответствующих указанным ролям.
+   */
+  @Override
+  public MyList<User> getUsersByRole(Role... roles) {
+    return null;
+  }
+
+
+  /**
+   * Находит и возвращает пользователя по заданному адресу электронной почты.
+   *
+   * @param email Электронная почта пользователя для поиска.
+   * @return Объект пользователя {@code User}, если найден; {@code null} иначе.
+   */
+  @Override
+  public User getUserByEmail(String email) {
+    return null;
+  }
+
 }
