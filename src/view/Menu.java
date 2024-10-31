a5b32ce86cf5efea984c681ec20ddf022ad2e151
s-saratov <saratovs@yandex.ru>
2024-10-31 12:12:11 +0100

    Menu methods add

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,4 +1,28 @@
 package view;
 
 public class Menu {
+
+
+    private void waitRead() {
+        System.out.print("\nДля продолжения нажмите Enter...");
+        scanner.nextLine();
+    }
+
+    // Возвращает только введённое число (предотвращая ошибку в случае ввода символов)
+
+    private int getSelection() {
+        int selection;
+        while (true) {
+            System.out.printf("Введите Ваш выбор: ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Вы ввели не число. Повторите ввод.");
+                scanner.nextLine();
+            } else {
+                selection = scanner.nextInt();
+                scanner.nextLine();
+                return selection;
+            }
+        }
+    }
+
 }
