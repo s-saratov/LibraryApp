cb4123c14cdb7fd9c0938b552c720e3613914e2a
s-saratov <saratovs@yandex.ru>
2024-10-31 12:14:13 +0100

    Menu improve

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,6 +1,17 @@
 package view;
 
+import service.MainService;
+
+import java.util.Scanner;
+
 public class Menu {
+
+    private final MainService service;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public Menu(MainService service) {
+        this.service = service;
+    }
 
 
     private void waitRead() {
