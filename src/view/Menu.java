35b394ab61ac3af2b075802ae519344f22e26771
OlehDemishev <oleh.demishev@gmail.com>
2024-10-31 13:01:36 +0100

    Oleh Menu

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -13,14 +13,12 @@ public class Menu {
         this.service = service;
     }
 
-
     private void waitRead() {
         System.out.print("\nДля продолжения нажмите Enter...");
         scanner.nextLine();
     }
 
     // Возвращает только введённое число (предотвращая ошибку в случае ввода символов)
-
     private int getSelection() {
         int selection;
         while (true) {
@@ -36,4 +34,46 @@ public class Menu {
         }
     }
 
+    // Главное меню
+    private void showMainMenu() {
+        System.out.println("Добро пожаловать в библиотеку 'Знания века'!!");
+        System.out.println("1. Меню книг -> showBookMenu()");
+        System.out.println("2. Меню пользователя -> showUserMenu()");
+        System.out.println("3. Меню администратора -> showAdminMenu()");
+        System.out.println("0. Выход из системы");
+
+        switch (getSelection()) {
+            case 1:
+                showBookMenu();
+                break;
+            case 2:
+                showUserMenu();
+                break;
+            case 3:
+                showAdminMenu();
+                break;
+            case 0:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Некорректный выбор. Повторите ввод.");
+                showMainMenu();
+                break;
+        }
+    }
+
+    private void showBookMenu() {
+        System.out.println("Здесь будет меню книг.");
+        waitRead();
+    }
+
+    private void showUserMenu() {
+        System.out.println("Здесь будет меню пользователя.");
+        waitRead();
+    }
+
+    private void showAdminMenu() {
+        System.out.println("Здесь будет меню администратора.");
+        waitRead();
+    }
 }
